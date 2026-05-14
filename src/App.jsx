0a59a568c1405e7d1620c6ad8f82b566adb74558
Mayara Soares <141755987+maydreadful@users.noreply.github.com>
2026-05-14 14:18:19 -0300

    criando as funções atualizar, remover e criar lista

--- src/App.jsx
+++ src/App.jsx
@@ -25,16 +25,43 @@ function App() {
     },
   ])
 
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+{
+  id: Math.floor(Math.random() * 1000),
+      text,
+      category,
+      isCompleted: false,
+},
+    ];
+      
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+        const filteredTodos = newTodos.filter((todo) => 
+            todo.id !== id? todo : null);
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => 
+      todo.id === id? todo.isCompleted = !todo.isCompleted: todo);
+    setTodos(newTodos);
+  };
+
   return (
     <div className='app'>
       <h1>Minha Lista de Tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo) => (
-          <Todo todo={todo}/>
-          
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
         ))}
       </div>
-      <TodoForm />
+      <TodoForm AddTodo={addTodo} />
     </div>
   );
 }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import { useState } from 'react';
 
-const TodoForm = () => {
-  return
-    <div className='todo-form'>
-        <h2>Criar Tarefa</h2>
-        <form>
-            <input type='text' placeholder='Digite o título...' />
-            <select>
-                <option value="">Selecione uma categoria</option>
-                <option value='Trabalho'>Trabalho</option>
-                <option value='Pessoal'>Pessoal</option>
-                <option value='Estudos'>Estudos</option>
-            </select>
-            <button type='submit'>Criar Tarefa</button>
-        </form>
-    </div>;
+const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) return;
+        addTodo(value, category);
+        setValue('');
+        setCategory('');
+    };
+
+    const removeTodo = (id) => {
+        // Lógica para remover a tarefa com o ID fornecido
+    const newTodos = [...todos]
+        const filteredTodos = newTodos.filter((todo) => 
+            todo.id !== id? todo : null);
+    setTodos(filteredTodos);
+    };
+
+    return (
+        <div className='todo-form'>
+            <h2>Criar Tarefa</h2>
+            <form onSubmit={handleSubmit}>
+                <input type='text' placeholder='Digite o título...' value={value} onChange={(e) => setValue(e.target.value)}/>
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="">Selecione uma categoria</option>
+                    <option value='Trabalho'>Trabalho</option>
+                    <option value='Pessoal'>Pessoal</option>
+                    <option value='Estudos'>Estudos</option>
+                </select>
+                <button type='submit'>Criar Tarefa</button>
+            </form>
+        </div>
+    );
 };
+
 export default TodoForm;
